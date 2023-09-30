3e37675c53c922d0c06f8fbef205d5b7d27dba99
WadekarPrashant <wadekarprashant733@gmail.com>
2023-09-30 17:38:21 +0530

    fresh start

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAJm9Nx_KsB2ZaOwVs5xjUKBFBXkyaWzpo",
+    authDomain: "a2z-basket.firebaseapp.com",
+    projectId: "a2z-basket",
+    storageBucket: "a2z-basket.appspot.com",
+    messagingSenderId: "373060417642",
+    appId: "1:373060417642:web:5186a21a3e5b5a5e247213",
+    measurementId: "G-FF7GWWLEGJ"
+  };
